a275a4e1b41e4f7927e71a0e52caab01de455994
Glib Petrachkov <petrasha@gmail.com>
2022-02-06 19:13:59 +0200

    Практика 2 задачки

--- Practica_8/Practica_8.cpp
+++ Practica_8/Practica_8.cpp
@@ -3,11 +3,44 @@
 
 //Задание 1. Вывести на экран прямоугольник с заданными сторонами заданным символом.
 
+//#include <iostream>
+//#include <Windows.h>
+//using namespace std;
+//int main()
+//{
+//	cout << "Enter the number for character between 0 to 255 - ";
+//	int s;
+//	cin >> s;
+//	char star = s; // 15 is sun
+//	cout << star << endl;
+//	cout << "Enter the number for length - ";
+//	int l = 8;
+//	cin >> l;
+//	cout << "Enter the number for height - ";
+//	int h = 10;
+//	cin >> h;
+//	
+//	for (int y = 0; y < h; y++)
+//	{
+//		for (int x = 0; x < l; x++)
+//		{
+//			cout << star;
+//		}
+//		cout << endl;
+//	}
+//}
+
+//Задание 2. Вывести на экран пустой прямоугольник с заданными размерами заданным символом заданного цвета.
+
 #include <iostream>
 #include <Windows.h>
 using namespace std;
 int main()
 {
+	
+	int color = 6; // 
+	cout << "Enter the number for color - ";
+	cin >> color;
 	cout << "Enter the number for character between 0 to 255 - ";
 	int s;
 	cin >> s;
@@ -19,18 +52,18 @@ int main()
 	cout << "Enter the number for height - ";
 	int h = 10;
 	cin >> h;
-	
+
 	for (int y = 0; y < h; y++)
 	{
 		for (int x = 0; x < l; x++)
 		{
+			HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+			SetConsoleTextAttribute(h, color);
 			cout << star;
 		}
 		cout << endl;
 	}
 }
-
-//Задание 2. Вывести на экран пустой прямоугольник с заданными размерами заданным символом заданного цвета.
 
 // Тема: Массивы одномерные
 // Задания для самостоятельной работы 2
