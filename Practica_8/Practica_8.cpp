0f21f17b1c50994419f375cfdf4d8481dc231fdf
Glib Petrachkov <petrasha@gmail.com>
2022-02-08 21:06:50 +0200

    practice done

--- Practica_8/Practica_8.cpp
+++ Practica_8/Practica_8.cpp
@@ -70,20 +70,24 @@
 //Задание 1. Написать, программу, которая вычисляет прибыль фирмы за 6 месяцев. Пользователь вводит прибыль
 //фирмы за каждый месяц
 
-#include <iostream>
-#include <Windows.h>
-using namespace std;
-int main()
-{
-	const int size = 6;
-	float month[size] = {2013.23, 1543.46, 1789.37, 8076.18, 9765.57, 12345};
-	float total = 0;
-	for (int i = 0; i < size; i++)
-	{
-		total += month[i];
-	}
-	cout << total << " UAH\n";
-}
+//#include <iostream>
+//#include <Windows.h>
+//using namespace std;
+//int main()
+//{
+//	const int size = 6;
+//	float month[size] = {2013.23, 1543.46, 1789.37, 8076.18, 9765.57, 12345};
+//
+//	float total = 0;
+//	for (int i = 0; i < size; i++)
+//	{
+//		cout << "Enter your earning for " << i + 1 << " month.\n";
+//		cin >> month[i];
+//		cout << " UAH\n";
+//		total += month[i];
+//	}
+//	cout << "Total icome for 6 months - " << total << " UAH\n";
+//}
 
 //Задание 2. Написать программу, которая выводит одномерный массив в обратном порядке.
 
@@ -101,6 +105,45 @@ int main()
 
 //Задание 3. Пользователь вводит длину сторон пятиугольника, каждая сторона заноситься в массив, необходимо
 //вычислить периметр пятиугольника(периметр — сумма всех сторон).
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	const int size = 5;
+//	float side[size];
+//	float sum = 0;
+//	for (int i = 0; i < size; i++)
+//	{
+//		cout << "Enter the metres of side " << i + 1 << " of pentagon. \n";
+//		cin >> side[i];
+//		sum += side[i];
+//	}
+//	cout << "Perimetr of pentagon - " << sum << " metres";
+//}
 	
-//Задание 4. Пользователь вводит прибыль фирмы за год (12 месяцев).Необходимо определить месяц, в котором
+//Задание 4. Пользователь вводит прибыль фирмы за год (12 месяцев). Необходимо определить месяц, в котором
 // прибыль была максимальна и месяц, в котором прибыль была минимальна.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	const int size = 12;
+	float month[size];
+	float max = 0;
+	float min = 0;
+	for (int i = 0; i < size; i++)
+	{
+		cout << "Enter income of month " << i + 1 << "\n";
+		cin >> month[i];
+		if (month[i] > max && i == 0) min = month[i], max = month[i];
+		else if (month[i] > max) max = month[i];
+		else if (month[i] < min) min = month[i];
+
+	}
+	cout << "Minimum incame of - " << min << "UAH\n";
+	cout << "Maximum income of - " << max << "UAH\n";
+}
+
+
