8ed9255d38926bd1dba2a8f86923865835b5bb05
Glib Petrachkov <petrasha@gmail.com>
2022-02-08 20:23:47 +0200

    problem 1 and 2

--- Practica_8/Practica_8.cpp
+++ Practica_8/Practica_8.cpp
@@ -32,46 +32,72 @@
 
 //Задание 2. Вывести на экран пустой прямоугольник с заданными размерами заданным символом заданного цвета.
 
+//#include <iostream>
+//#include <Windows.h>
+//using namespace std;
+//int main()
+//{
+//	
+//	int color = 6; // 
+//	cout << "Enter the number for color - ";
+//	cin >> color;
+//	cout << "Enter the number for character between 0 to 255 - ";
+//	int s;
+//	cin >> s;
+//	char star = s; // 15 is sun
+//	cout << star << endl;
+//	cout << "Enter the number for length - ";
+//	int l = 8;
+//	cin >> l;
+//	cout << "Enter the number for height - ";
+//	int h = 10;
+//	cin >> h;
+//	for (int y = 0; y < h; y++)
+//	{
+//		for (int x = 0; x < l; x++)
+//		{
+//			HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+//			SetConsoleTextAttribute(h, color);
+//			cout << star;
+//		}
+//		cout << endl;
+//	}
+//}
+
+// Тема: Массивы одномерные
+// Задания для самостоятельной работы 2
+
+//Задание 1. Написать, программу, которая вычисляет прибыль фирмы за 6 месяцев. Пользователь вводит прибыль
+//фирмы за каждый месяц
+
 #include <iostream>
 #include <Windows.h>
 using namespace std;
 int main()
 {
-	
-	int color = 6; // 
-	cout << "Enter the number for color - ";
-	cin >> color;
-	cout << "Enter the number for character between 0 to 255 - ";
-	int s;
-	cin >> s;
-	char star = s; // 15 is sun
-	cout << star << endl;
-	cout << "Enter the number for length - ";
-	int l = 8;
-	cin >> l;
-	cout << "Enter the number for height - ";
-	int h = 10;
-	cin >> h;
-
-	for (int y = 0; y < h; y++)
+	const int size = 6;
+	float month[size] = {2013.23, 1543.46, 1789.37, 8076.18, 9765.57, 12345};
+	float total = 0;
+	for (int i = 0; i < size; i++)
 	{
-		for (int x = 0; x < l; x++)
-		{
-			HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
-			SetConsoleTextAttribute(h, color);
-			cout << star;
-		}
-		cout << endl;
+		total += month[i];
 	}
+	cout << total << " UAH\n";
 }
 
-// Тема: Массивы одномерные
-// Задания для самостоятельной работы 2
-
-//Задание 1. Написать, программу, которая вычисляет прибыль фирмы за 6 месяцев.Пользователь вводит прибыль
-//фирмы за каждый месяц.
-
 //Задание 2. Написать программу, которая выводит одномерный массив в обратном порядке.
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	const int size = 10;
+//	int num[size] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//	for ( int i = 0; i < size; i++)
+//	{
+//		cout << num[size - 1 - i] << endl;
+//	}
+//}
 
 //Задание 3. Пользователь вводит длину сторон пятиугольника, каждая сторона заноситься в массив, необходимо
 //вычислить периметр пятиугольника(периметр — сумма всех сторон).
